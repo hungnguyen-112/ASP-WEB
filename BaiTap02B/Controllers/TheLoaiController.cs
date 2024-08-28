a65474d570aba8e5e20afff5b34f5884e7463a5e
hungnguyen-112 <hungnhh112@gmail.com>
2024-08-28 14:36:39 +0700

    Hoan Thanh BaiTap02

---
+++ BaiTap02B/Controllers/TheLoaiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap02B.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
