b5200950ebf1837559168c0c38f35e6ceb22c17c
hungnguyen-112 <hungnhh112@gmail.com>
2024-09-14 14:34:18 +0700

    Delete BaiTap7 directory

--- BaiTap7/Models/SanPhamViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BaiTap7.Models
-{
-    public class SanPhamViewModel
-    {
-        public string TenSanPham { get; set; }
-        public decimal GiaBan { get; set; }
-        public string AnhMoTa { get; set; }
-    }
-}
