4201ec72c3c9b414581bfce523b1daf0372b5625
hungnguyen-112 <hungnhh112@gmail.com>
2024-09-14 15:00:07 +0700

    hoan thanh BaiTapKiemTra01

---
+++ BaiTapKiemTra01/Controllers/SanPhamController.cs
@@ -0,0 +1,24 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BaiTap2()
+        {
+            var sanpham = new SanPhamViewModel()
+            {
+                TenSanPham = "Điện thoại iPhone 14 Pro Max",
+                GiaBan = 33990000,
+                AnhMoTa = "/images/tải_xuống-removebg-preview.png"
+            };
+
+            return View(sanpham);
+        }
+    }
+}
