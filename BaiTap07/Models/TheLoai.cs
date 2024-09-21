5d65f62ddc22e1db5c0d3043f35ec32e042471b8
hungnguyen-112 <hungnhh112@gmail.com>
2024-09-21 15:12:10 +0700

    Hoàn Thành BTVeNha

---
+++ BaiTap07/Models/TheLoai.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaiTap07.Models
+{
+    public class TheLoai
+    {
+    [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Không được để trống tên thể loại!")]
+        [Display(Name="Thể Loại")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Không định dạng đúng ngày!")]
+        [Display(Name = "Ngày Tạo")]
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
